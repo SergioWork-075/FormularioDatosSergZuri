2c821d282677ed25b97c9fded72ae9e7eb7c668b
ZuRi-To <91980222+ZuRi-To@users.noreply.github.com>
2021-11-12 19:39:53 +0100

    contra añadida :)

--- index.php
+++ index.php
@@ -1,8 +1,33 @@
 <?php
+/////////////EMAIL//////////////////////
 if (false !== filter_var($_GET["validacionEmail"], FILTER_VALIDATE_EMAIL)) {
     echo 'Bien introducido';
 } else {
     echo 'Mail mal introducido';
+}
+/////////////CONTRASEÑA//////////////////////
+$contra = filter_input(INPUT_GET, 'contra', FILTER_SANITIZE_STRING);
+$mayus = preg_match('@[A-Z]@', $contra);
+$minus = preg_match('@[a-z]@', $contra);
+$num = preg_match('@[0-9]@', $contra);
+$especial = preg_match('@[^\w]@', $contra);
+
+if (isset($_GET['Enviar'])) {
+    if (!$especial) {
+        echo "La contraseña debe tener un caracter especial!";
+    }
+    if(!$mayus || !$minus){
+        echo "<br>La contraseña debe tener MAYUS y MINUS!";
+    }
+    if(!$num){
+        echo "<br>La contraseña debe tener almenos un número!";
+    }
+    if( strlen($contra) < 8 || strlen($contra) > 16){
+        echo "<br>La contraseña debe tener entre 8 y 16 caracteres!";
+    }
+    else {
+        echo 'Strong password.';
+    }
 }
 
 ?>
@@ -22,7 +47,15 @@ if (false !== filter_var($_GET["validacionEmail"], FILTER_VALIDATE_EMAIL)) {
         <input name="validacionEmail" type="text" />
         <button type="submit" title="validacionEmail">enviar</button>
         <!--submit para enviar los datos-->
+        <h1>Contraseña</h1>
+        <form method="get" action="index.php">
+    <input type="text" name="contra"  value=""/>
+
+    <input type="submit" name="Enviar" />
+</form>
     </form>
 </body>
 
 </html>
+
+
