6c7cc62f180198be807973a45a721b3e16d21118
SergioWork-075 <sergy0754@gmail.com>
2021-11-12 19:36:56 +0100

    primera prueba comprobacion mail

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
-<?php 
-    
+<?php
+if (false !== filter_var($_GET["validacionEmail"], FILTER_VALIDATE_EMAIL)) {
+    echo 'Bien introducido';
+} else {
+    echo 'Mail mal introducido';
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario de Acceso</title>
 </head>
+
 <body>
     <h1>Formulario de Acceso</h1>
+    <form action="index.php" method="get">
+        <input name="validacionEmail" type="text" />
+        <button type="submit" title="validacionEmail">enviar</button>
+        <!--submit para enviar los datos-->
+    </form>
 </body>
+
 </html>
