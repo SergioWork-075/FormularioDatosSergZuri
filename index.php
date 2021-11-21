064b86f6632c5967245dcdafc8ec2098ef7d8654
ZuRi-To <91980222+ZuRi-To@users.noreply.github.com>
2021-11-21 18:36:14 +0100

    Provincias + CP arreglado!
    Importante: al pasar a limpio declarar arriba postal y provincia2

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
-
 <?php
+
+$provincia2 = "";
+$postal = "";
+
 /////LEER EL TXT////
 $meses = [];
 $string = file_get_contents("archivo.txt");
-$array = explode("\n",$string);
-foreach ($array as $fila){
-    $item = explode(" ",$fila);
+$array = explode("\n", $string);
+foreach ($array as $fila) {
+    $item = explode(" ", $fila);
     $meses[] = [
         'postal' => $item[0],
         'nombre' => $item[1]
@@ -40,23 +43,23 @@ $especial = preg_match('@[^\w]@', $contra);
 
 ////////////WEB///////////////////////
 $web = filter_input(INPUT_GET, 'validacionWeb', FILTER_SANITIZE_STRING);
-$webprueba= preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$web);
+$webprueba = preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $web);
 
 if (isset($_GET['Enviar'])) {
     /////COMPROBACION LONGITUD////
-    if(strlen($nombre)<2){
+    if (strlen($nombre) < 2) {
         echo "NOMBRE:Tiene que contener al menos 2 caracteres<br/>";
     }
-    if(strlen($apelli)<4){
+    if (strlen($apelli) < 4) {
         echo "APELLIDOS:Tiene que contener al menos 4 caracteres<br/>";
     }
-    if(strlen($ciudad)<4){
+    if (strlen($ciudad) < 4) {
         echo "CIUDAD:Tiene que contener al menos 4 caracteres<br/>";
     }
-    if(strlen($telef)>9){
+    if (strlen($telef) > 9) {
         echo "TELEFONO:No puede incluir mas de 9 numeros<br/>";
     }
-    if(strlen($postal)>5){
+    if (strlen($postal) > 5) {
         echo "TELEFONO:No puede incluir mas de 5 numeros<br/>";
     }
     /////COMPROBACION QUE NO ESCRIBA NUMEROS////
@@ -120,67 +123,78 @@ if (isset($_GET['Enviar'])) {
 </form>
 <form method="get" action="index.php">
     <h2>Nombre</h2>
-    <input name="nombre" type="text" />
+    <input name="nombre" type="text"/>
     <h2>Apellidos</h2>
-    <input name="apellido" type="text" />
+    <input name="apellido" type="text"/>
     <h2>Direccion</h2>
-    <input name="direccion" type="text" />
+    <input name="direccion" type="text"/>
     <h2>Ciudad</h2>
-    <input name="ciudad" type="text" />
+    <input name="ciudad" type="text"/>
     <h2>Provincia</h2>
     <?php
     $meses[] = [];
     $string = file_get_contents("./archivo2.txt");
-    $array = explode("\n",$string);
+    $array = explode("\n", $string);
 
-    $entry_order_type ="Selecte order type from the list and press the submit button";
-
-    if(isset($_POST['provincias'])){
-        $entry_order_type = $_GET['provincias'];
-    }
-
-
-    foreach ($array as $fila){
-        $item = explode(" ",$fila);
+    foreach ($array as $fila) {
+        $item = explode(" ", $fila);
 
         ?>
         <form action="#" method="get">
 
             <select name="provincias"> <?php
-                for ( $i = 1; $i < 104; $i++ ) {
-                    $meses += [ $i => $item[$i] ];
+                for ($i = 1; $i < 104; $i++) {
+                    $meses += [$i => $item[$i]];
                     if ($i % 2) {
-                        $_SESSION['provincia'] = $item[$i] . $_GET['provincias']; ?>
-                        <option value="<?php echo $item[$i-1] . $item[$i];?>"><?php echo $_SESSION['provincia'];?></option>
+                        $_SESSION['provincia'] = $item[$i]; ?>
+
+                        <option value="<?php echo $item[$i - 1] . " " . $item[$i]; ?>"><?php echo $_SESSION['provincia']; ?></option>
 
                         <?php
                     }
                 }
                 ?> </select>
-            <input type="submit" name="submit" value="Ver" />
+            <input type="submit" name="submit" value="Ver"/>
 
         </form> <?php
     }
-    ?><h2>Código postal</h2><?php
+    ?><?php
     if (isset($_GET['submit'])) {
         $codigo_prov = $_GET['provincias'];
-    ?><input name="postal" type="text" value="<?php echo $codigo_prov;?>"/><?php
-    }else{?><input name="postal" type="text" value=""/><?php }
-    ?>
 
+        $info[] = [];
+        $array2 = explode("\n", $codigo_prov);
+        foreach ($array2 as $selec) {
+            $valor = explode(" ", $selec);
+
+            for ($i = 0; $i < 2; $i++) {
+                $info += [$i => $valor[$i]];
+                if ($i % 2) {
+                    $provincia2 = $valor[$i];
+                } else {
+                    $postal = $valor[$i];
+                }
+
+            }
+        }
+    } ?><br>
+    <input name="provincias2" readonly type="text" value="<?php echo $provincia2; ?>"/><br>
+
+    <h2>Código postal</h2>
+    <input name="postal" type="text" value="<?php echo $postal; ?>"/>
 
     <h2>Telefono</h2>
-    <input name="telef" type="tel" />
+    <input name="telef" type="tel"/>
     <h2>Correo</h2>
-    <input name="validacionEmail" type="email" />
+    <input name="validacionEmail" type="email"/>
     <h2>Contraseña</h2>
-    <input type="password" name="contra" value="" />
+    <input type="password" name="contra" value=""/>
     <h2>Web</h2>
-    <input name="validacionWeb" type="text" />
+    <input name="validacionWeb" type="text"/>
 
-        <br>
+    <br>
 
-    <input type="submit" name="Enviar" />
+    <input type="submit" name="Enviar"/>
 
 </form>
 </body>
